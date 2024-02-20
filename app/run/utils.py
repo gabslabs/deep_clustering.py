e65497eb2d4f7a6c36c7ca64378205582c0a07c6
gaboche <fedou.gab@proton.me>
2024-02-20 19:11:16 +0100

    added data-description file

---
+++ app/run/utils.py
@@ -0,0 +1,37 @@
+import torch
+import numpy as np
+import matplotlib.pyplot as plt; plt.rcParams['figure.dpi'] = 200
+
+DEVICE = 'cpu'
+
+def train(autoencoder, data, epochs=20):
+    opt = torch.optim.Adam(autoencoder.parameters())
+    for epoch in range(epochs):
+        for x, y in data:
+            x = x.to(DEVICE)
+            opt.zero_grad()
+            x_hat = autoencoder(x)
+            loss = ((x - x_hat)**2).sum() + autoencoder.encoder.kl
+            loss.backward()
+            opt.step()
+    return autoencoder
+
+def plot_latent(autoencoder, data, num_batches=100):
+    for i, (x, y) in enumerate(data):
+        z = autoencoder.encoder(x.to(DEVICE))
+        z = z.to(DEVICE).detach().numpy()
+        plt.scatter(z[:, 0], z[:, 1], c=y, cmap='tab10')
+        if i > num_batches:
+            plt.colorbar()
+            break
+
+def plot_reconstructed(autoencoder, r0=(-5, 10), r1=(-10, 5), n=12):
+    w = 28
+    img = np.zeros((n*w, n*w))
+    for i, y in enumerate(np.linspace(*r1, n)):
+        for j, x in enumerate(np.linspace(*r0, n)):
+            z = torch.Tensor([[x, y]]).to(DEVICE)
+            x_hat = autoencoder.decoder(z)
+            x_hat = x_hat.reshape(28, 28).to(DEVICE).detach().numpy()
+            img[(n-1-i)*w:(n-1-i+1)*w, j*w:(j+1)*w] = x_hat
+    plt.imshow(img, extent=[*r0, *r1])
